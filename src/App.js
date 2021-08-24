5cea0421af410d3e2dee8a2f483ba4f5b5c1db66
Teklehaimanot Belete <enbish.blt@gmail.com>
2021-08-24 10:20:55 +0300

    banner layout

--- src/App.js
+++ src/App.js
@@ -1,17 +1,12 @@
 import { FaAlignRight } from 'react-icons/fa';
 import './App.scss';
 import Header from './componont/header/Header';
+import Banner from './componont/banner/Banner'
 function App() {
   return (
     <div className="App">
       <Header />
-      <div className='main-banner'>
-        <div className='main-banner-content'>
-          <span>Embish Fashion 2021</span>
-          <h1>men and women style collection</h1>
-          <p>around bole Emperial +251938278416</p>
-        </div>
-      </div>
+      <Banner/>
     </div>
   );
 }
