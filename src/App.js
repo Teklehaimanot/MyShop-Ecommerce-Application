8beb81f00a5dcb0cacb729f3c151884c87ef4818
Teklehaimanot Belete <enbish.blt@gmail.com>
2021-08-24 10:20:16 +0300

    creating a banner for promotional purpose

--- src/App.js
+++ src/App.js
@@ -6,7 +6,11 @@ function App() {
     <div className="App">
       <Header />
       <div className='main-banner'>
-        
+        <div className='main-banner-content'>
+          <span>Embish Fashion 2021</span>
+          <h1>men and women style collection</h1>
+          <p>around bole Emperial +251938278416</p>
+        </div>
       </div>
     </div>
   );
---
+++ src/componont/banner/Banner.js
@@ -0,0 +1,14 @@
+import './Banner.scss'
+const Banner = () => {
+    return (
+        <div className='main-banner'>
+            <div className='main-banner-content'>
+                <span>Embish Fashion 2021</span>
+                <h1>men and women style collection</h1>
+                <p>around bole Emperial +251938278416</p>
+            </div>
+        </div>
+    )
+}
+
+export default Banner
