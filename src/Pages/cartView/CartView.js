7c498339c0c06ca15097f1faa8a7b554af10fd2b
Teklehaimanot Belete <enbish.blt@gmail.com>
2021-09-09 16:52:45 +0300

    adding proceed to checkout dialog box on the cart page

--- src/Pages/cartView/CartView.js
+++ src/Pages/cartView/CartView.js
@@ -1,3 +1,6 @@
+import { FaMinus } from 'react-icons/fa';
+import { FaPlus } from 'react-icons/fa';
+import { RiDeleteBin6Line } from 'react-icons/ri'
 import './CartView.scss'
 const CartView = () => {
     return (
@@ -14,29 +17,145 @@ const CartView = () => {
                     <div className='cart-table-col cart-table-col-img'>
                         <div className='cart-table-col-img'></div>
                     </div>
-                    <div className='cart-table-col'></div>
-                    <div className='cart-table-col'></div>
-                    <div className='cart-table-col'></div>
-                    <div className='cart-table-col'></div>
+                    <div className='cart-table-col name'>
+                        <a href="#"> Women Red t-shirt </a>
+                        <ul>
+                            <li>Color: <strong>red</strong></li>
+                            <li>Size: <strong>XL</strong></li>
+                            <li>Material: <strong>Cotton</strong></li>
+                        </ul>
+                    </div>
+                    <div className='cart-table-col'>
+                        <span>$190</span>
+                    </div>
+                    <div className='cart-table-col'>
+                        <div className='quantity'>
+                            <span><FaMinus /></span>
+                            <span>1</span>
+                            <span><FaPlus /></span>
+                        </div>
+
+                    </div>
+                    <div className='cart-table-col'>
+                        <div className='total'>
+                            <span>$190</span>
+                            <a href="#"> <RiDeleteBin6Line /></a>
+                        </div>
+                    </div>
                 </div>
                 <div className='cart-table-item'>
                     <div className='cart-table-col'>
                         <div className='cart-table-col-img'></div>
                     </div>
-                    <div className='cart-table-col'></div>
-                    <div className='cart-table-col'></div>
-                    <div className='cart-table-col'></div>
-                    <div className='cart-table-col'></div>
+                    <div className='cart-table-col name'>
+                        <a href="#"> Women Red t-shirt </a>
+                        <ul>
+                            <li>Color: <strong>red</strong></li>
+                            <li>Size: <strong>XL</strong></li>
+                            <li>Material: <strong>Cotton</strong></li>
+                        </ul>
+                    </div>
+                    <div className='cart-table-col'>
+                        <span>$190</span>
+                    </div>
+                    <div className='cart-table-col'>
+                        <div className='quantity'>
+                            <span><FaMinus className='quantity-icon' /></span>
+                            <span>1</span>
+                            <span><FaPlus className='quantity-icon' /></span>
+                        </div>
+                    </div>
+
+                    <div className='cart-table-col'>
+                        <div className='total'>
+                            <span>$190</span>
+                            <a href="#"> <RiDeleteBin6Line /></a>
+                        </div>
+                    </div>
+                </div>
+                <div className='cart-table-item'>
+                    <div className='cart-table-col'>
+                        <div className='cart-table-col-img'></div>
+                    </div>
+                    <div className='cart-table-col name'>
+                        <a href="#"> Women Red t-shirt </a>
+                        <ul>
+                            <li>Color: <strong>red</strong></li>
+                            <li>Size: <strong>XL</strong></li>
+                            <li>Material: <strong>Cotton</strong></li>
+                        </ul>
+                    </div>
+                    <div className='cart-table-col'>
+                        <span>$190</span>
+                    </div>
+                    <div className='cart-table-col'>
+                        <div className='quantity'>
+                            <span><FaMinus className='quantity-icon' /></span>
+                            <span>1</span>
+                            <span><FaPlus className='quantity-icon' /></span>
+                        </div>
+                    </div>
+
+                    <div className='cart-table-col'>
+                        <div className='total'>
+                            <span>$190</span>
+                            <a href="#"> <RiDeleteBin6Line /></a>
+                        </div>
+                    </div>
+                </div>
+                <div className='cart-table-item'>
+                    <div className='cart-table-col'>
+                        <div className='cart-table-col-img'></div>
+                    </div>
+                    <div className='cart-table-col name'>
+                        <a href="#"> Women Red t-shirt </a>
+                        <ul>
+                            <li>Color: <strong>red</strong></li>
+                            <li>Size: <strong>XL</strong></li>
+                            <li>Material: <strong>Cotton</strong></li>
+                        </ul>
+                    </div>
+                    <div className='cart-table-col'>
+                        <span>$190</span>
+                    </div>
+                    <div className='cart-table-col'>
+                        <div className='quantity'>
+                            <span><FaMinus className='quantity-icon' /></span>
+                            <span>1</span>
+                            <span><FaPlus className='quantity-icon' /></span>
+                        </div>
+                    </div>
+
+                    <div className='cart-table-col'>
+                        <div className='total'>
+                            <span>$190</span>
+                            <a href="#"> <RiDeleteBin6Line /></a>
+                        </div>
+                    </div>
+                </div>
+
+                <div className='cart-table-item'>
+                    <div className='cart-table-item-bottom'>
+                        <a href="#">CONTINUE SHOPING</a>
+                        <label htmlFor="">
+                            <input type="checkbox" />
+                            <span>Shipping(+6$)</span>
+                        </label>
+                    </div>
+
                 </div>
             </div>
 
-            {/* <div className='cart-button'>
-                hey
-            </div>
+            <div className= 'cart-total'>
+                <h3>Cart Total</h3>
+                <ul>
+                    <li>Subtotal: <span>$1040</span></li>
+                    <li>Shipping: <span>$6</span></li>
+                    <li>Total: <span>$1046</span></li>
+                </ul>
+                <a href="#">PROCEED TO CHECKOUT</a>
 
-            <div className='cart-totals'>
-                hey
-            </div> */}
+            </div>
         </div>
     )
 }
