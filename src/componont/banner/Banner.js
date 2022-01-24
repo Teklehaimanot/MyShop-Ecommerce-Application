0354e398c7ce8dea7295feb1b81ef704baa3688a
Teklehaimanot Belete <enbish.blt@gmail.com>
2022-01-24 12:02:42 +0300

    changed title in the banner

--- src/componont/banner/Banner.js
+++ src/componont/banner/Banner.js
@@ -3,7 +3,7 @@ const Banner = () => {
     return (
         <div className='main-banner'>
             <div className='main-banner-content'>
-                <span>Embish Fashion 2021</span>
+                <span>My-Shop Fashion 2021</span>
                 <h1>men and women style collection</h1>
                 <p>around bole Emperial +251938278416</p>
             </div>
