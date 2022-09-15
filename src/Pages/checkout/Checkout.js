0773d70aab89e56bc3c6bc03c38d6697f7d3d586
Teklehaimanot Belete <enbish.blt@gmail.com>
2022-09-15 11:25:46 +0300

    handling onSubbimt method on the checkout class

--- src/Pages/checkout/Checkout.js
+++ src/Pages/checkout/Checkout.js
@@ -6,6 +6,16 @@ import './Checkout.scss'
 const Checkout = () => {
     const { carts, shippingValue } = useContext(cartContext)
     const [showPaymentCard, setShowPaymentCard] = useState(false)
+    const [error, setErro] = useState('')
+    const [billingForm, setBillingForm] = useState({
+        country: '',
+        firstName: '',
+        lastName: '',
+        address: '',
+        city: '',
+        email: '',
+        phoneNumber: ''
+    })
 
     const Subtotal = () => {
         const total = carts.reduce((acc, curr) => {
@@ -17,16 +27,36 @@ const Checkout = () => {
     const total = () => {
         return shippingValue + Subtotal()
     }
+
+    const handleSubmit = (e) => {
+        e.preventDefault()
+        const { country, firstName, lastName, address, city, email, phoneNumber } = billingForm
+        if (country && firstName && lastName && address && city && email && phoneNumber) {
+            setShowPaymentCard(true)
+        } else (
+            setErro('Please fill all the required fileds')
+        )
+
+    }
+
+    const handleChange = (e) => {
+        const { name, value } = e.target
+        setBillingForm((prev) => {
+            return { ...prev, [name]: value }
+        })
+    }
+
+    const { country, firstName, lastName, address, city, email, phoneNumber } = billingForm
     return (
         <>
             {
-                !showPaymentCard ? <form>
+                !showPaymentCard ? <form onSubmit={handleSubmit}>
                     <div className='checkout-main'>
                         <div className='billing-details'>
                             <h3>Billing Details</h3>
                             <div className='form-group'>
                                 <label>Country <span class="required">*</span></label>
-                                <select className='form-control' name="country">
+                                <select className='form-control' name="country" value={country} onChange={handleChange}>
                                     <option value="6">Ethiopia</option>
                                     <option value="5">United Arab Emirates</option>
                                     <option value="1">China</option>
@@ -39,30 +69,30 @@ const Checkout = () => {
                             <div className='full-name'>
                                 <div className='form-group first-name'>
                                     <label>First Name <span class="required">*</span></label>
-                                    <input type='text' />
+                                    <input type='text' name='firstName' value={firstName} onChange={handleChange} />
                                 </div>
                                 <div className='form-group last-name'>
                                     <label>Last Name <span class="required">*</span></label>
-                                    <input type='text' />
+                                    <input type='text' name='lastName' value={lastName} onChange={handleChange} />
                                 </div>
                             </div>
                             <div className='form-group'>
                                 <label>Address <span class="required">*</span></label>
-                                <input type='text' />
+                                <input type='text' name='address' value={address} onChange={handleChange} />
                             </div>
                             <div className='form-group'>
                                 <label>Town/City <span class="required">*</span></label>
-                                <input type='text' />
+                                <input type='text' name='city' value={city} onChange={handleChange} />
                             </div>
 
                             <div className='full-name'>
                                 <div className='form-group first-name'>
                                     <label>Email <span >*</span></label>
-                                    <input type='email' />
+                                    <input type='email' name='email' value={email} onChange={handleChange} />
                                 </div>
                                 <div className='form-group last-name'>
                                     <label>Phone Number <span>*</span></label>
-                                    <input type='text' />
+                                    <input type='text' name='phoneNumber' value={phoneNumber} onChange={handleChange} />
                                 </div>
                             </div>
                         </div>
@@ -98,11 +128,11 @@ const Checkout = () => {
                             </table>
                             <hr />
                             <input type='submit' value='PLACE ORDER' />
+                            <span style={{ color: 'red', paddingTop: '3rem' }}>{error}</span>
                         </div>
                     </div>
                 </form> : <StripeContainer />
             }
-
 
         </>
 
